bab5b768aec4982d2477f513dbf15da090152299
ingniq <ingniq8@gmail.com>
2020-08-25 21:50:24 +0300

    improve tests

--- ll/tests.py
+++ ll/tests.py
@@ -52,34 +52,56 @@ class TestLinkedListMethods(unittest.TestCase):
         self.assertEqual(actual, expected)
 
     def test_find(self):
+        # поиск значения
         NODE_VAL = 12
         node = self.linked_list.find(NODE_VAL)
         self.assertEqual(node.value, NODE_VAL)
 
+        # поиск несуществующего значения
         NODE_VAL = 128
         node = self.linked_list.find(NODE_VAL)
         self.assertIsNone(node)
 
-        NODE_VAL = '12'
-        node = self.linked_list.find(NODE_VAL)
-        self.assertIsNone(node)
-
+        # поиск по None
         NODE_VAL = None
+        self.linked_list.add_in_tail(Node(NODE_VAL))
         node = self.linked_list.find(NODE_VAL)
-        self.assertIsNone(node)
+        self.assertEqual(node.value, NODE_VAL)
 
+        # поиск по "сложному" типу тданных
         NODE_VAL = ['test', 12]
         self.linked_list.add_in_tail(Node(NODE_VAL))
         node = self.linked_list.find(NODE_VAL)
         self.assertEqual(node.value, NODE_VAL)
 
+        # поиск по нулевому значению
+        NODE_VAL = 0
+        self.linked_list.add_in_tail(Node(NODE_VAL))
+        node = self.linked_list.find(NODE_VAL)
+        self.assertEqual(node.value, NODE_VAL)
+
+        # поиск в пустом списке
+        linked_list = LinkedList()
+        node = linked_list.find(12)
+        self.assertIsNone(node)
+
+        # поиск в списке из одного узла
+        linked_list.add_in_tail(Node(12))
+        node = linked_list.find(12)
+        self.assertEqual(node.value, 12)
+
+        node = linked_list.find(10)
+        self.assertIsNone(node)
+
     def test_find_all(self):
+        # поиск значения
         NODE_VAL = 12
         nodes = self.linked_list.find_all(NODE_VAL)
         self.assertEqual(len(nodes), 2)
         for node in nodes:
             self.assertEqual(node.value, NODE_VAL)
 
+        # поиск по "сложному" типу тданных
         NODE_VAL = ['test', 12]
         self.linked_list.add_in_tail(Node(NODE_VAL))
         nodes = self.linked_list.find_all(NODE_VAL)
@@ -87,16 +109,40 @@ class TestLinkedListMethods(unittest.TestCase):
         for node in nodes:
             self.assertEqual(node.value, NODE_VAL)
 
+        # поиск несуществующего значения
         NODE_VAL = 120
         nodes = self.linked_list.find_all(NODE_VAL)
         self.assertEqual(len(nodes), 0)
 
+        # получение всех узлов списка
         nodes = self.linked_list.find_all()
         self.assertEqual(len(nodes), 4)
 
+        # поиск по None
         NODE_VAL = None
-        nodes = self.linked_list.find_all()
+        nodes = self.linked_list.find_all(NODE_VAL)
         self.assertEqual(len(nodes), 4)
+
+        # поиск по нулевому значению
+        NODE_VAL = 0
+        self.linked_list.add_in_tail(Node(NODE_VAL))
+        nodes = self.linked_list.find_all(NODE_VAL)
+        self.assertEqual(len(nodes), 1)
+
+        # поиск в пустом списке
+        linked_list = LinkedList()
+        nodes = linked_list.find_all(12)
+        self.assertEqual(len(nodes), 0)
+
+        # поиск в списке из одного узла
+        linked_list.add_in_tail(Node(12))
+        nodes = linked_list.find_all(12)
+        self.assertEqual(len(nodes), 1)
+        for node in nodes:
+            self.assertEqual(node.value, 12)
+
+        nodes = linked_list.find_all(10)
+        self.assertEqual(len(nodes), 0)
 
     def test_delete(self):
         # удаление первого найденого элемента из нескольких найденых
@@ -199,24 +245,65 @@ class TestLinkedListMethods(unittest.TestCase):
         self.assertEqual(self.linked_list.len(), 0)
 
     def test_insert(self):
-        NODE_VALUE = 1200
-        self.linked_list.insert(None, Node(NODE_VALUE))
-        self.assertEqual(self.linked_list.head.value, NODE_VALUE)
+        # вставка в список со многими элементами в начало списка
+        self.linked_list.insert(None, Node(1200))
+        self.assertEqual(self.linked_list.head.value, 1200)
+        self.assertEqual(self.linked_list.head.next.value, 12)
         self.assertEqual(self.linked_list.len(), 4)
+        self.assertEqual(self.linked_list.tail.value, 12)
 
-        NODE_VALUE = 55
-        node = self.linked_list.find(NODE_VALUE)
-        self.linked_list.insert(node, Node(NODE_VALUE + 11))
-        self.assertEqual(node.next.value, NODE_VALUE + 11)
+        # вставка в список со многими элементами
+        node = self.linked_list.find(55)
+        self.linked_list.insert(node, Node(66))
+        self.assertEqual(node.next.value, 66)
         self.assertEqual(self.linked_list.len(), 5)
+        self.assertEqual(self.linked_list.head.value, 1200)
+        self.assertEqual(self.linked_list.tail.value, 12)
 
+        # вставка в список отличного от Node элемента
         NODE_VALUE = 100
         self.linked_list.insert(node, NODE_VALUE)
         self.assertEqual(self.linked_list.len(), 5)
+        self.assertEqual(self.linked_list.head.value, 1200)
+        self.assertEqual(self.linked_list.tail.value, 12)
 
+        # вставка в список после несуществующего элемента
+        linked_list = LinkedList()
         NODE_VALUE = 100
+        linked_list.add_in_tail(Node(1))
+        linked_list.add_in_tail(Node(2))
+        linked_list.add_in_tail(Node(3))
         self.linked_list.insert(Node(1500), NODE_VALUE)
-        self.assertEqual(self.linked_list.len(), 5)
+        self.assertEqual(linked_list.len(), 3)
+        self.assertEqual(linked_list.head.value, 1)
+        self.assertEqual(linked_list.tail.value, 3)
+
+        # вставка в пустой список
+        linked_list = LinkedList()
+        linked_list.insert(None, Node(NODE_VALUE))
+        node = linked_list.find(NODE_VALUE)
+        self.assertEqual(linked_list.len(), 1)
+        self.assertEqual(node.value, NODE_VALUE)
+        self.assertEqual(node.next, None)
+        self.assertEqual(linked_list.head.value, NODE_VALUE)
+        self.assertEqual(linked_list.tail.value, NODE_VALUE)
+
+        # вставка в пустой список после несуществующего элемента
+        linked_list = LinkedList()
+        linked_list.insert(Node(10), Node(10))
+        self.assertEqual(linked_list.len(), 0)
+        self.assertIsNone(linked_list.head)
+        self.assertIsNone(linked_list.tail)
+
+        # вставка в список с одним элементом и проверка на нулевое значение
+        linked_list = LinkedList()
+        linked_list.insert(None, Node(0))
+        node = linked_list.find(0)
+        self.assertEqual(linked_list.len(), 1)
+        self.assertEqual(node.value, 0)
+        self.assertEqual(node.next, None)
+        self.assertEqual(linked_list.head.value, 0)
+        self.assertEqual(linked_list.tail.value, 0)
 
     def test_validate_node(self):
         self.assertTrue(self.linked_list._validate_node(Node(123)))
