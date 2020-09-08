580051f370fceaec417da9d80d22e11fe2b0ff8a
ingniq <ingniq8@gmail.com>
2020-09-08 09:33:41 +0300

    lesson 4: add tests

---
+++ stack/tests.py
@@ -0,0 +1,34 @@
+import unittest
+from stack import Stack
+
+
+class TestStackMethods(unittest.TestCase):
+    def test_methods(self):
+        stack = Stack()
+        # test push
+        stack.push(1)
+        # test peek
+        self.assertEqual(stack.peek(), 1)
+        # test size
+        self.assertEqual(stack.size(), 1)
+
+        stack.push("2")
+        self.assertEqual(stack.peek(), "2")
+        self.assertEqual(stack.size(), 2)
+
+        stack.push(3.14)
+        self.assertEqual(stack.size(), 3)
+        self.assertEqual(stack.peek(), 3.14)
+
+        # test pop
+        self.assertEqual(stack.pop(), 3.14)
+        self.assertEqual(stack.peek(), "2")
+        self.assertEqual(stack.size(), 2)
+
+        self.assertEqual(stack.pop(), "2")
+        self.assertEqual(stack.peek(), 1)
+        self.assertEqual(stack.size(), 1)
+
+        self.assertEqual(stack.pop(), 1)
+        self.assertEqual(stack.peek(), None)
+        self.assertEqual(stack.size(), 0)
