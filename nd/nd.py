05de3a125c19f0df46d217d7fff033b484a70d3e
ingniq <ingniq8@gmail.com>
2020-11-08 18:35:43 +0300

    lesson 9: native dict

---
+++ nd/nd.py
@@ -0,0 +1,98 @@
+class NativeDictionary:
+    def __init__(self, sz):
+        self.size = sz
+        self.slots = [None] * self.size
+        self.values = [None] * self.size
+
+    def hash_fun(self, key: str):
+        # в качестве value поступают строки!
+        if not isinstance(key, str):
+            return None
+
+        return sum(key.encode("utf-8")) % self.size
+
+    def is_key(self, key):
+        # возвращает True если ключ имеется,
+        # иначе False
+        # index = self.hash_fun(key)
+        if not isinstance(key, str):
+            return False
+
+        index = self.find(key)
+
+        if index is not None:
+            return True
+
+        return False
+
+    def seek_slot(self, value: str):
+        if not isinstance(value, str):
+            return None
+
+        index = self.hash_fun(value)
+
+        if self.slots[index] is None:
+            return index
+
+        return self.__find_loop(index, None)
+
+    def put(self, key: str, value: str):
+        # гарантированно записываем
+        # значение value по ключу key
+
+        if not isinstance(key, str) or not isinstance(value, str):
+            return None
+
+        # update value
+        if self.is_key(key):
+            key_index = self.find(key)
+            self.values[key_index] = value
+            return
+
+        key_index = self.seek_slot(key)
+
+        if key_index is not None:
+            self.slots[key_index] = key
+            self.values[key_index] = value
+
+    def get(self, key):
+        # возвращает value для key,
+        # или None если ключ не найден
+
+        if not isinstance(key, str):
+            return None
+
+        key_index = self.find(key)
+
+        return self.values[key_index] if key_index is not None else None
+
+    def find(self, value: str):
+        if not isinstance(value, str):
+            return None
+
+        index = self.hash_fun(value)
+
+        if self.slots[index] is None:
+            return None
+
+        if self.slots[index] == value:
+            return index
+
+        return self.__find_loop(index, value)
+
+    def __find_loop(self, index, value):
+        next_index = index + 1
+
+        if next_index >= self.size:
+            next_index -= self.size
+
+        while next_index != index:
+            if self.slots[next_index] == value:
+                return next_index
+
+            next_index += 1
+
+            if next_index >= self.size:
+                next_index -= self.size
+
+        return None
---
+++ nd/tests.py
@@ -0,0 +1,198 @@
+import unittest
+from nd import NativeDictionary
+from random import randrange
+
+
+class TestNativeDictionaryMethods(unittest.TestCase):
+    def test_hash_fun(self):
+        size = 17
+        nd = NativeDictionary(size)
+
+        value = "string 123"
+        index = nd.hash_fun(value)
+        self.assertEqual(12, index)
+
+        # коллизия
+        value = "321 gnirts"
+        index = nd.hash_fun(value)
+        self.assertEqual(12, index)
+
+        # не строка
+        value = 123
+        index = nd.hash_fun(value)
+        self.assertIsNone(index)
+
+        # не строка
+        value = [123]
+        index = nd.hash_fun(value)
+        self.assertIsNone(index)
+
+    def test_seek_slot(self):
+        size = 17
+        nd = NativeDictionary(size)
+
+        keys = [
+            "string",
+            "gnirts",
+            "string1",
+            "string2",
+            "string3"
+            ]
+
+        for v in keys:
+            nd.put(v, v + " " + str(randrange(nd.size)))
+
+        # без коллизий
+        index = nd.seek_slot("string4")
+        self.assertEqual(3, index)
+
+        # с учетом коллизий
+        index = nd.seek_slot(keys[0])
+        self.assertEqual(3, index)
+        index = nd.seek_slot(keys[1])
+        self.assertEqual(3, index)
+        index = nd.seek_slot(keys[2])
+        self.assertEqual(3, index)
+        index = nd.seek_slot(keys[3])
+        self.assertEqual(3, index)
+        index = nd.seek_slot(keys[4])
+        self.assertEqual(3, index)
+
+        nd.put("string15", "string15 Test")
+
+        index = nd.seek_slot(keys[0])
+        self.assertEqual(4, index)
+        index = nd.seek_slot(keys[1])
+        self.assertEqual(4, index)
+        index = nd.seek_slot(keys[4])
+        self.assertEqual(4, index)
+
+        # не строка
+        value = 123
+        index = nd.seek_slot(value)
+        self.assertIsNone(index)
+
+    def test_put(self):
+        size = 17
+        nd = NativeDictionary(size)
+
+        nd.put("string", str(randrange(nd.size)))
+        index = nd.find("string")
+        self.assertEqual(0, index)
+        # колллизия
+        nd.put("gnirts", str(randrange(nd.size)))
+        index = nd.find("gnirts")
+        self.assertEqual(1, index)
+        nd.put("string1", str(randrange(nd.size)))
+        index = nd.find("string1")
+        self.assertEqual(15, index)
+        nd.put("string2", str(randrange(nd.size)))
+        index = nd.find("string2")
+        self.assertEqual(16, index)
+        # колллизия
+        nd.put("string3", str(randrange(nd.size)))
+        index = nd.find("string3")
+        self.assertEqual(2, index)
+
+        # не строка
+        value = 123
+        nd.put(value, str(randrange(nd.size)))
+        index = nd.find(value)
+        self.assertIsNone(index)
+
+    def test_get(self):
+        size = 17
+        nd = NativeDictionary(size)
+
+        values = {
+            "string": "value 1",
+            "gnirts": "value 2",
+            "string1": "value 3",
+            "string2": "value 4",
+            "string3": "value 5"
+        }
+
+        for i in values:
+            nd.put(i, values[i])
+
+        # без коллизий
+        value = nd.get("string")
+        self.assertEqual("value 1", value)
+        value = nd.get("string1")
+        self.assertEqual("value 3", value)
+        value = nd.get("string2")
+        self.assertEqual("value 4", value)
+
+        # с учетом коллизий
+        value = nd.get("gnirts")
+        self.assertEqual("value 2", value)
+        value = nd.get("string3")
+        self.assertEqual("value 5", value)
+
+        # не строка
+        value = 123
+        value = nd.get(value)
+        self.assertIsNone(value)
+
+    def test_find(self):
+        size = 17
+        nd = NativeDictionary(size)
+
+        keys = [
+            "string",
+            "gnirts",
+            "string1",
+            "string2",
+            "string3"
+            ]
+
+        for v in keys:
+            nd.put(v, str(randrange(nd.size)))
+
+        # без коллизий
+        index = nd.find(keys[0])
+        self.assertEqual(0, index)
+        index = nd.find(keys[2])
+        self.assertEqual(15, index)
+        index = nd.find(keys[3])
+        self.assertEqual(16, index)
+
+        # с учетом коллизий
+        index = nd.find(keys[1])
+        self.assertEqual(1, index)
+        index = nd.find(keys[4])
+        self.assertEqual(2, index)
+
+        # не строка
+        value = 123
+        index = nd.find(value)
+        self.assertIsNone(index)
+
+    def test_is_key(self):
+        size = 17
+        nd = NativeDictionary(size)
+
+        keys = [
+            "string",
+            "gnirts",
+            "string1",
+            "string2",
+            "string3"
+            ]
+
+        for v in keys:
+            nd.put(v, str(randrange(nd.size)))
+
+        self.assertFalse(nd.is_key("test string"))
+
+        # без коллизий
+        self.assertTrue(nd.is_key(keys[0]))
+        self.assertTrue(nd.is_key(keys[2]))
+        self.assertTrue(nd.is_key(keys[3]))
+
+        # с учетом коллизий
+        self.assertTrue(nd.is_key(keys[1]))
+        self.assertTrue(nd.is_key(keys[4]))
+
+        # не строка
+        self.assertFalse(nd.is_key(123))
