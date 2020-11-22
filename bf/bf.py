2f9c9ccd109a3b3f6d1ba64001af909436e219de
ingniq <ingniq8@gmail.com>
2020-11-22 13:43:03 +0300

    lesson 10: bites array

--- bf/bf.py
+++ bf/bf.py
@@ -2,7 +2,7 @@ class BloomFilter:
 
     def __init__(self, f_len):
         self.filter_len = f_len
-        self.filter = bytearray([0] * f_len)
+        self.filter = [0] * f_len
 
     def hash1(self, str1):
         RAND_CONST = 17
