ee29dc0615571008c02ae188964d9877c4f75493
Igor Filatov <siqniq@bk.ru>
2023-05-12 21:47:16 +0300

    tree implementation

---
+++ tree/simple_tree.py
@@ -0,0 +1,109 @@
+class SimpleTreeNode:
+
+    def __init__(self, val, parent):
+        self.NodeValue = val
+        self.Parent = parent
+        self.Children = []
+        self.level = 0
+
+class SimpleTree:
+
+    def __init__(self, root: SimpleTreeNode) -> None:
+        self.Root = root
+
+    def AddChild(self, ParentNode: SimpleTreeNode, NewChild: SimpleTreeNode) -> None:
+        if not ParentNode or not NewChild:
+            return None
+
+        ParentNode.Children.append(NewChild)
+        NewChild.Parent = ParentNode
+        self.update_node_levels(NewChild)
+
+    def DeleteNode(self, NodeToDelete: SimpleTreeNode) -> None:
+        if NodeToDelete == self.Root:
+            return None
+
+        NodeToDelete.Parent.Children.remove(NodeToDelete)
+        NodeToDelete.Parent = None
+
+    def GetAllNodes(self) -> list:
+        if self.Root is None:
+            return []
+
+        return [self.Root] + self.__find_nodes(self.Root.Children, 0, None)
+
+    def FindNodesByValue(self, val) -> list:
+        if self.Root is None:
+            return []
+
+        found = []
+
+        if self.Root.NodeValue == val:
+            found.append(self.Root)
+
+        return found + self.__find_nodes(self.Root.Children, 0, val)
+
+    def __find_nodes(self, nodes: list, index: int, val) -> list:
+        if index >= len(nodes):
+            return []
+
+        found = []
+
+        if val is not None and nodes[index].NodeValue == val:
+            found.append(nodes[index])
+        elif val is None:
+            found.append(nodes[index])
+
+        if nodes[index].Children:
+            found += self.__find_nodes(nodes[index].Children, 0, val)
+
+        return found + self.__find_nodes(nodes, index + 1, val)
+
+    def MoveNode(self, OriginalNode: SimpleTreeNode, NewParent: SimpleTreeNode) -> None:
+        if self.Root is None or OriginalNode == self.Root:
+            return None
+
+        self.DeleteNode(OriginalNode)
+        self.AddChild(NewParent, OriginalNode)
+        self.update_node_levels(OriginalNode)
+
+    def update_node_levels(self, start_node: SimpleTreeNode) -> None:
+        self.__update_node_levels_recursively([start_node], 0)
+
+    def __update_node_levels_recursively(self, nodes: list, index: int) -> None:
+        if index >= len(nodes):
+            return None
+
+        if nodes[index].Parent is None:
+            nodes[index].level = 0
+        else:
+            nodes[index].level = nodes[index].Parent.level + 1
+
+        if nodes[index].Children:
+            self.__update_node_levels_recursively(nodes[index].Children, 0)
+
+        return self.__update_node_levels_recursively(nodes, index + 1)
+
+    def Count(self) -> int:
+        if self.Root is None:
+            return 0
+        return self.__items_counter(nodes=[self.Root], index=0, only_leaves=False)
+
+    def LeafCount(self) -> int:
+        if self.Root is None:
+            return 0
+        return self.__items_counter(nodes=[self.Root], index=0, only_leaves=True)
+
+    def __items_counter(self, *, nodes: list, index: int, only_leaves: bool) -> int:
+        if index >= len(nodes):
+            return 0
+
+        num_items = 0
+
+        if not only_leaves or not nodes[index].Children:
+            num_items += 1
+
+        if nodes[index].Children:
+            num_items += self.__items_counter(nodes=nodes[index].Children, index=0, only_leaves=only_leaves)
+
+        return num_items + self.__items_counter(nodes=nodes, index=index + 1, only_leaves=only_leaves)
---
+++ tree/simple_tree_tests.py
@@ -0,0 +1,168 @@
+import unittest
+from simple_tree import SimpleTree, SimpleTreeNode
+
+class TestSimpleTreeMethods(unittest.TestCase):
+    def test_methods(self):
+        # create tree
+        root_node = SimpleTreeNode(0, None)
+        tree = SimpleTree(root_node)
+        self.assertEqual(tree.Root, root_node)
+        self.assertIsNone(tree.Root.Parent)
+
+        tree = SimpleTree(None)
+        tree.AddChild(tree.Root, root_node)
+        self.assertIsNone(tree.Root)
+        self.assertIsNone(root_node.Parent)
+
+        tree.Root = root_node
+
+        self.assertEqual(tree.Root, root_node)
+        self.assertEqual(root_node.level, 0)
+        self.assertEqual(len(root_node.Children), 0)
+
+        # add clildren (first level)
+        node_1 = SimpleTreeNode(1, None)
+        node_2 = SimpleTreeNode(2, None)
+        node_3 = SimpleTreeNode(3, None)
+        tree.AddChild(root_node, node_1)
+        tree.AddChild(root_node, node_2)
+        tree.AddChild(root_node, node_3)
+
+        self.assertIsNone(tree.Root.Parent)
+        self.assertEqual(len(tree.Root.Children), 3)
+        self.assertEqual(node_1.Parent, root_node)
+        self.assertEqual(node_2.Parent, root_node)
+        self.assertEqual(node_3.Parent, root_node)
+        self.assertIn(node_1, tree.Root.Children)
+        self.assertIn(node_2, tree.Root.Children)
+        self.assertIn(node_3, tree.Root.Children)
+        self.assertEqual(node_1.level, 1)
+        self.assertEqual(node_2.level, 1)
+        self.assertEqual(node_3.level, 1)
+
+        # add clildren (second level)
+        node_4 = SimpleTreeNode(4, None)
+        node_5 = SimpleTreeNode(5, None)
+        tree.AddChild(node_1, node_4)
+        tree.AddChild(node_2, node_5)
+
+        self.assertEqual(len(node_1.Children), 1)
+        self.assertEqual(len(node_2.Children), 1)
+        self.assertEqual(node_4.Parent, node_1)
+        self.assertEqual(node_5.Parent, node_2)
+        self.assertIn(node_4, node_1.Children)
+        self.assertIn(node_5, node_2.Children)
+        self.assertEqual(node_4.level, 2)
+        self.assertEqual(node_5.level, 2)
+
+        # add clildren (third level)
+        node_6 = SimpleTreeNode(6, None)
+        tree.AddChild(node_4, node_6)
+
+        self.assertIn(node_6, node_4.Children)
+        self.assertEqual(node_6.Parent, node_4)
+        self.assertEqual(node_6.level, 3)
+
+        # add clildren (second level)
+        node_7 = SimpleTreeNode(7, None)
+        node_8 = SimpleTreeNode(8, None)
+        tree.AddChild(node_3, node_7)
+        tree.AddChild(node_3, node_8)
+
+        self.assertEqual(len(node_3.Children), 2)
+        self.assertEqual(node_7.Parent, node_3)
+        self.assertEqual(node_8.Parent, node_3)
+        self.assertIn(node_7, node_3.Children)
+        self.assertIn(node_8, node_3.Children)
+        self.assertEqual(node_7.level, 2)
+
+        # root
+        # | \ \
+        # 1 2 3
+        # | | | \
+        # 4 5 7 8
+        # |
+        # 6
+
+        # DeleteNode
+        parent = node_1.Parent
+        tree.DeleteNode(node_1)
+        self.assertNotIn(node_1, parent.Children)
+        self.assertIsNone(node_1.Parent)
+
+        tree.DeleteNode(tree.Root)
+        self.assertIsNotNone(tree.Root)
+        self.assertEqual(len(tree.Root.Children), 2)
+
+        # root
+        # | \
+        # 2 3
+        # | | \
+        # 5 7 8
+
+        # GetAllNodes
+        nodes = tree.GetAllNodes()
+        self.assertListEqual(nodes, [tree.Root, node_2, node_5, node_3, node_7, node_8])
+
+        # FindNodesByValue
+        node_9 = SimpleTreeNode(2, None)
+        node_10 = SimpleTreeNode(2, None)
+        node_11 = SimpleTreeNode(-1, None)
+        tree.AddChild(node_8, node_9)
+        tree.AddChild(node_5, node_10)
+        tree.AddChild(node_5, node_11)
+
+        # root
+        # | \
+        # 2 3
+        # | | \
+        # 5 7 8
+        # | \ |
+        # 2-1 2
+
+        nodes = tree.FindNodesByValue(2)
+        self.assertListEqual(nodes, [node_2, node_10, node_9])
+
+        nodes = tree.FindNodesByValue(8)
+        self.assertListEqual(nodes, [node_8])
+
+        nodes = tree.FindNodesByValue(0)
+        self.assertListEqual(nodes, [tree.Root])
+
+        nodes = tree.FindNodesByValue(-1)
+        self.assertListEqual(nodes, [node_11])
+
+        nodes = tree.FindNodesByValue(-10)
+        self.assertListEqual(nodes, [])
+
+        # MoveNode
+        tree.MoveNode(node_5, tree.Root)
+        self.assertNotIn(node_5, node_2.Children)
+        self.assertIn(node_5, tree.Root.Children)
+        self.assertEqual(node_5.Parent, tree.Root)
+        self.assertEqual(node_5.level, 1)
+        self.assertEqual(node_10.level, 2)
+        self.assertEqual(node_11.level, 2)
+
+        tree.MoveNode(node_8, node_11)
+        self.assertNotIn(node_8, node_3.Children)
+        self.assertIn(node_8, node_11.Children)
+        self.assertEqual(node_8.Parent, node_11)
+        self.assertEqual(node_8.level, 3)
+        self.assertEqual(node_9.level, 4)
+
+        # root
+        # | \ \
+        # 2 3 5
+        #   | | \
+        #   7 2 -1
+        #        |
+        #        8
+        #        |
+        #        2
+
+        # Count
+        self.assertEqual(tree.Count(), 9)
+
+        # LeafCount
+        self.assertEqual(tree.LeafCount(), 4)
