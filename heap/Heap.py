19e0ecc1c16728579d2a01be67c9e8a4d2d36fa9
Igor Filatov <siqniq@bk.ru>
2023-05-22 20:01:46 +0600

    heap: implementation start

---
+++ heap/Heap.py
@@ -0,0 +1,13 @@
+class Heap:
+
+    def __init__(self):
+        self.HeapArray = [] # non-negative numbers
+
+    def MakeHeap(self, a: list, depth: int) -> None:
+        pass
+
+    def GetMax(self) -> int:
+    	return -1
+
+    def Add(self, key: int) -> bool:
+    	return False
